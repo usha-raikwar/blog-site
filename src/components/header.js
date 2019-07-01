dc6c5c8f27a9f6ba8c78c3cb5573d5fa89a9a03b
usha raikwar <usharaikwar@gmail.com>
2019-07-01 16:09:01 +0530

    added favicon

--- src/components/header.js
+++ src/components/header.js
@@ -15,8 +15,13 @@ const Header = ({ siteTitle }) => (
       <Link to="/blog">Blog</Link>
       <Link to="/contact">Contact</Link>
     </nav>
-    <link href="https://fonts.googleapis.com/css?family=Cormorant+Upright|Lora|Prata&display=swap" rel="stylesheet">
-    </link>
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Upright|Lora|Prata&display=swap" rel="stylesheet" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
   </header>
 );
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const Contact = () => (
     <h2>Contact</h2>
     <p>
         Open for cool opportunities. Got any idea, suggestion or for collaboration?
-        Reach me @ <strong>usharaikwar@gmail.com</strong>
+        Reach out to me @ <strong>usharaikwar@gmail.com</strong>
     </p>
     </div>
   </Layout>
